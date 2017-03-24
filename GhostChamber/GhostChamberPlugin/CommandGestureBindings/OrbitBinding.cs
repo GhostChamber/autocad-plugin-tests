4312ab663a31c863752de2c451e2bdf4b91478c7
Gaurav Nanda <gauravnba@gmail.com>
2017-03-24 14:18:46 -0500

    The orbit gesture is now in the negative direction of real world movement. For better 1 to 1 feel of the gesture.

--- GhostChamber/GhostChamberPlugin/CommandGestureBindings/OrbitBinding.cs
+++ GhostChamber/GhostChamberPlugin/CommandGestureBindings/OrbitBinding.cs
@@ -15,8 +15,8 @@ namespace GhostChamberPlugin.CommandGestureBindings
         private OrbitCommand command = new OrbitCommand();
         private OrbitGesture gesture = new OrbitGesture();
 
-        private const double X_ROTATION_MULTIPLIER = 3.0;
-        private const double Y_ROTATION_MULTIPLIER = 1.5;
+        private const double X_ROTATION_MULTIPLIER = -3.0;
+        private const double Y_ROTATION_MULTIPLIER = -1.5;
 
         public bool IsGestureActive(IList<Body> skeletons, int bodyCount)
         {
@@ -28,7 +28,7 @@ namespace GhostChamberPlugin.CommandGestureBindings
             Vector3d movement = gesture.Update(skeletons, bodyCount);
             if (movement.Length > 0.0)
             {
-                command.Do(Vector3d.ZAxis, -1*movement.X * X_ROTATION_MULTIPLIER);
+                command.Do(Vector3d.ZAxis, movement.X * X_ROTATION_MULTIPLIER);
                 command.Do(Vector3d.XAxis, movement.Y * Y_ROTATION_MULTIPLIER);
             }
         }
